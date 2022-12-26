e452988b66852fca6595aceadc16f1ec86d3b2e7
Alicia Parra <alicia@redparra.com>
2022-12-26 20:38:32 +0100

    Add axios and fetch test at index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,61 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
+import axios, { AxiosError } from 'axios';
 
-const inter = Inter({ subsets: ['latin'] })
-
+type Datatype = {
+  accessibility: Number,
+  activity: String,
+  participants: Number,
+  type: String,
+  price: Number,
+  link: String
+}
 export default function Home() {
+  const [data,setData] = useState<Datatype>();
+  const [axiosData,setAxiosData] = useState<Datatype>();
+  useEffect(() => {
+  
+  //USING FETCH
+  const dataFetch = async () => {
+    try{
+      const response =  await fetch("http://www.boredapi.com/api/activity/")
+
+      //I have to check the response to thrown an error when http response is 404
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+      }
+  
+      const data = await response.json()
+       // set state when the data received
+      setData(data);
+    }
+    catch (error: any){
+      const message = `An error has occured: ${error.message}`;
+      throw new Error(message);
+    }
+   
+
+  };
+  dataFetch();
+
+  // USING AXIOS
+  const dataAxios= async () => {
+    try {
+      const response = await axios.get('http://www.boredapi.com/api/activity/');
+      setAxiosData(response.data);
+    } catch (error) {
+      const err = error as AxiosError
+      const message = `An error has occured: ${err.message}`;
+      throw new Error(message);
+    }
+  }
+
+  dataAxios()
+},[]);
+
   return (
     <>
       <Head>
@@ -15,108 +66,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
+        <div>
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+            <h1>Activity data fetched with fetch</h1>
+            <p>{data ? data.activity : ""}</p>
+            <p><b>Type:</b> {data ? data.type : ""}</p>
+          </div>
+          <div>
+            <h1>Activity data fetched with axios</h1>
+            <p>{axiosData ? axiosData.activity : ""}</p>
+            <p><b>Type:</b> {axiosData ? axiosData.type : ""}</p>
           </div>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+     
       </main>
     </>
   )
